1a838a14ee1b1777e02e42e21c0655d719f650b3
Gasarayev <gasarayev.emil@gmail.com>
2024-11-14 22:41:20 +0400

    certificates

--- js/swiper.js
+++ js/swiper.js
@@ -21,3 +21,32 @@ var swiper = new Swiper(".mySwiper", {
     },
   },
 });
+
+var swiper = new Swiper(".mySwiperCertificate", {
+  slidesPerView: 4,
+  spaceBetween: 30,
+  loop: true,
+  breakpoints: {
+    250: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+    640: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    991: {
+      slidesPerView: 4,
+      spaceBetween: 20,
+    },
+  },
+  autoplay: {
+    delay: 3000,
+  },
+});
+
+
