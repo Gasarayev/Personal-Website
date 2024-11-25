9e0a20f9613a32304c34c207c24d0ad16468e938
Gasarayev <gasarayev.emil@gmail.com>
2024-11-25 14:35:54 +0400

    feat: update routs

--- my-mvp-backend/server.js
+++ my-mvp-backend/server.js
@@ -1,63 +1,54 @@
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
 const app = express();
 const PORT = 3000;
-const engine = require("ejs-mate"); // Импортируем ejs-mate
+const engine = require("ejs-mate");
 
-
-// Укажите, что будем использовать EJS
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-
-
-// Укажите папку для статических файлов (CSS, JS, изображения)
-app.use(express.static(path.join(__dirname, "public")));
 app.engine("ejs", engine);
 
-// Маршруты
-app.get("/", (req, res) => {
-    try {
-      res.render("index");
-    } catch (err) {
-      console.error(err);
-      res.status(500).send("Error rendering the page");
-    }
-  });
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/contact", (req, res) => {
-  res.render("contact"); // Использует views/contact.ejs
+function getBlogs() {
+  const dataPath = path.join(__dirname, 'db.json');
+  const data = fs.readFileSync(dataPath);
+  const jsonData = JSON.parse(data);
+  return jsonData.blogs;
+}
+
+app.get("/", (req, res) => {
+  res.render("index");
 });
 
 app.get("/portfolio", (req, res) => {
-  res.render("portfolio"); // Использует views/portfolio.ejs
+  res.render("portfolio");
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact");
 });
 
 app.get("/blog", (req, res) => {
-  const blogs = [
-    { id: 1, title: "First Blog", content: "Content of the first blog." },
-    { id: 2, title: "Second Blog", content: "Content of the second blog." },
-  ];
-  res.render("blog", { blogs }); // Передаем данные в шаблон
+  const blogs = getBlogs();
+  res.render("blog", { blogs });
 });
 
 app.get("/blog/:id", (req, res) => {
-  const blogs = [
-    { id: 1, title: "First Blog", content: "Content of the first blog." },
-    { id: 2, title: "Second Blog", content: "Content of the second blog." },
-  ];
+  const blogs = getBlogs();
   const blog = blogs.find((b) => b.id === parseInt(req.params.id));
   if (!blog) {
     return res.status(404).send("Blog not found");
   }
-  res.render("blog-detail", { blog }); // Передаем данные блога
+  console.log(blog); 
+  res.render("blog-details", { blog });
 });
 
-// Middleware для обработки 404
 app.use((req, res) => {
-  res.status(404).render("404"); // Создайте views/404.ejs
+  res.status(404).render("404");
 });
 
-// Запуск сервера
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
